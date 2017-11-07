ec7761e1c9adba0e4dc28cf0b7b6799cba9e2fc8
cameron macfarlane <cammac1984@gmail.com>
2017-11-07 15:03:48 -0800

    testing updates, debugging

--- src/Admin/AdminServiceProvider.php
+++ src/Admin/AdminServiceProvider.php
@@ -22,14 +22,10 @@ class AdminServiceProvider extends ServiceProvider {
 
         $this->publishes([
             __DIR__.'/../config/admin.php' => config_path('admin.php'),
-            __DIR__.'/../resources/assets' => resource_path('assets/admin'),
-            __DIR__.'/../resources/assets/images' => storage_path('app/public'),
         ]);
 
         $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'admin');
-        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        $this->app->make(Factory::class)->load(__DIR__ . '/../database/factories');
 
         $this->app->bind('Admin', function()
         {
