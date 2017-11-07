fe479a36d10dc4633c2bf54a812b883c1fabe9c5
cameron macfarlane <cammac1984@gmail.com>
2017-11-07 15:26:55 -0800

    cleanup

--- src/Admin/AdminServiceProvider.php
+++ src/Admin/AdminServiceProvider.php
@@ -27,10 +27,6 @@ class AdminServiceProvider extends ServiceProvider {
         $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'admin');
 
-        $this->app->bind('Admin', function()
-        {
-            return new Jameron\Admin\Admin;
-        });
 
     }
 }
