bafebe152345b96f3dcb3af35337a03db0cc2432
cameron macfarlane <cammac1984@gmail.com>
2017-11-15 10:56:05 -0800

    cleanued view

--- src/resources/views/partials/forms/sign_in.blade.php
+++ src/resources/views/partials/forms/sign_in.blade.php
@@ -3,22 +3,16 @@
 
     <h2 class="form-signin-heading">Please sign in</h2>
     <label for="inputEmail" class="sr-only">Email address</label>
-    <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="">
+    <input type="email" name="email" value="{{ old('email') }}" id="email" class="form-control" placeholder="Email address" required="" autofocus="">
 
-    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-        <label for="email" class="sr-only">E-Mail Address</label>
-        <input type="email" id="email" class="form-control" placeholder="Email address" name="email" value="{{ old('email') }}" required="" autofocus="">
-    </div>
+    <label for="inputPassword" class="sr-only">Password</label>
+    <input type="password" id="password" class="form-control" name="password" placeholder="Password" required="">
 
     @if ($errors->has('email'))
         <span class="help-block">
             <strong>{{ $errors->first('email') }}</strong>
         </span>
     @endif
-
-    <label for="inputPassword" class="sr-only">Password</label>
-    <input type="password" id="password" class="form-control" name="password" placeholder="Password" required="">
-
     @if ($errors->has('password'))
         <span class="help-block">
             <strong>{{ $errors->first('password') }}</strong>
