d8cc2bea36cf6b27535cbfe0e5e0006c9da708be
cameron macfarlane <cammac1984@gmail.com>
2017-11-15 11:36:32 -0800

    Added clean canvas for the dash view

--- src/resources/views/admin/dash.blade.php
+++ src/resources/views/admin/dash.blade.php
@@ -5,16 +5,6 @@
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">Dashboard</div>
-                <div class="panel-body">
-                    @if(file_exists(base_path() . '/vendor/jameron/regulator/'))
-                    <a href="{{ url('/admin/users') }}" class="btn btn-primary">Manage Users</a>
-                    <a href="{{ url('/admin/roles') }}" class="btn btn-primary">Manage Roles</a>
-                    <a href="{{ url('/admin/permissions') }}" class="btn btn-primary">Manage Permissions</a>                
-                    @endif
-                    @if(file_exists(base_path() . '/vendor/jameron/invitations/'))
-                    <a href="{{ url('/admin/invitations') }}" class="btn btn-primary">Manage Invitations</a>
-                    @endif
-                </div>
             </div>
         </div>
     </div>
