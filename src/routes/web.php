577289df4c5659c6f808e7892b340e5e9719a2b6
cameron macfarlane <cammac1984@gmail.com>
2017-11-17 15:14:32 -0800

    -m

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,6 +2,6 @@
 
 Route::group(['middleware' => ['web', 'auth', 'role:admin']], function () {
     Route::get('admin', 'Jameron\Admin\Http\Controllers\Admin\AdminController@showAdminDashboard');
-    Route::get('admin/settings', 'Jameron\Admin\Http\Controllers\Admin\SettingsController@edit');
-    Route::post('admin/settings', 'Jameron\Admin\Http\Controllers\Admin\SettingsController@update');
+    Route::get('/settings', 'Jameron\Admin\Http\Controllers\Admin\SettingsController@edit');
+    Route::post('/settings', 'Jameron\Admin\Http\Controllers\Admin\SettingsController@update');
 });
