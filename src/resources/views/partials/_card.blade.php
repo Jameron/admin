9a453870bbd99f7790fc0aee3dbdb4007f3305b3
cameron macfarlane <cammac1984@gmail.com>
2017-12-19 15:08:01 -0800

    added card partial

---
+++ src/resources/views/partials/_card.blade.php
@@ -0,0 +1,14 @@
+<div class="container-fluid">
+    <div class="row justify-content-md-left">
+        <div class="col-12 col-md-12 col-lg-12">
+            <div class="card" style="margin-top: 1rem;">
+                <h4 class="card-header">
+                {{ $header }}
+                </h4>
+                <div class="card-body">
+                    {{ $body }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
