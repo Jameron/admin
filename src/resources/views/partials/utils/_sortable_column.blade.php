663de90396a41bd581d75522746105b54e12db2b
cameron macfarlane <cammac1984@gmail.com>
2017-12-20 17:23:00 -0800

    fixin the column up

--- src/resources/views/partials/utils/_sortable_column.blade.php
+++ src/resources/views/partials/utils/_sortable_column.blade.php
@@ -1,14 +1,14 @@
 <th data-column="{{ $column }}">
 	@if($column==$sort_by && $order=='desc')
-        <a href="{{ $url or '#' }}&order=asc#table" class="btn-sort"><span><i class="fa fa-angle-up" aria-hidden="true"></i> </span></a>
+        <a href="#" class="btn-sort"><span><i class="fa fa-angle-up" aria-hidden="true" onclick="document.getElementById('sort_by').value='{{$column}}';document.getElementById('order').value='asc';document.getElementById('{{ $form_id or '' }}').submit();"></i> </span></a>
 	@elseif($column==$sort_by && $order=='asc')
-        <a href="{{ $url or '#' }}&order=desc#table" class="btn-sorted"><span><i class="fa fa-angle-down" aria-hidden="true"></i></span></a>
+        <a href="#" class="btn-sorted"><span><i class="fa fa-angle-down" aria-hidden="true" onclick="document.getElementById('sort_by').value='{{$column}}';document.getElementById('order').value='desc';document.getElementById('{{ $form_id or '' }}').submit();"></i></span></a>
 	@endif
 	@if($column==$sort_by && $order=='desc')
-	<span class="sort-title"><a href="{{ $url or '#' }}&order=asc#table" class="btn-sort">{{ $th }}</a></span>
+	<span class="sort-title"><a href="#" class="btn-sort" onclick="document.getElementById('sort_by').value='{{$column}}';document.getElementById('order').value='asc';document.getElementById('{{ $form_id or '' }}').submit();">{{ $th }}</a></span>
 	@elseif($column==$sort_by && $order=='asc')
-	<span class="sort-title"><a href="{{ $url or '#' }}&order=desc#table" class="btn-sort">{{ $th }}</a></span>
+	<span class="sort-title"><a href="#" class="btn-sort" onclick="document.getElementById('sort_by').value='{{$column}}';document.getElementById('order').value='desc';document.getElementById('{{ $form_id or '' }}').submit();">{{ $th }}</a></span>
 	@else
-	<span class="sort-title"><a href="{{ $url or '#' }}&order=asc#table" class="btn-sort">{{ $th }}</a></span>
+	<span class="sort-title"><a href="#" class="btn-sort" onclick="document.getElementById('sort_by').value='{{$column}}';document.getElementById('order').value='asc';document.getElementById('{{ $form_id or '' }}').submit();">{{ $th }}</a></span>
 	@endif
 </th>
