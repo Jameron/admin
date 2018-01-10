59369d8954016f26e6c76c90ddd533786ffc79c1
cameron macfarlane <cammac1984@gmail.com>
2018-01-10 11:17:39 -0800

    Added in index view partial

---
+++ src/resources/views/partials/utils/_index.blade.php
@@ -0,0 +1,67 @@
+@if($search['show'])
+    <div class="row">
+        <div class="col-md-9">
+            @include('admin::partials.forms._search', ['search'=> $search])
+            <p class="subtle float-right mt-2">Displaying {!! $items->firstItem() !!} - {!! $items->lastItem() !!} of  {!! $items->total() !!} total</p>
+            @if(!empty($search['string']))
+                <a href="{{ url($resource_route) }}">Clear Search</a>
+            @endif
+        </div>
+    </div>
+@endif
+<table class="table table-hover table-responsive table-striped">
+    <thead>
+        <tr>
+            @foreach($columns as $key => $column)
+                @include('admin::partials.utils._sortable_column', 
+                [
+                    'th' => $column['label'], 
+                    'url' => url($resource_route . '?sortBy=' . $column['column'] . '&search=' . $search['string']), 
+                    'column' => $column['column']
+                    ])
+                @endforeach
+                @include('admin::partials.utils._sortable_column', 
+                [
+                    'th' => 'Option(s)', 
+                    'url' => null, 
+                    'column' => null 
+                    ])
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($items as $item)
+            <tr>
+                @foreach($columns as $key => $column)
+                    <td data-column="{{ $column['column'] }}">
+                        @if(isset($column['link']))
+                            @if(isset($column['link']) && isset($column['link']['resource_route']))
+                                <a href="{{ url($column['link']['resource_route'] . '/' . $item->{$column['link']['id_column']} ) }}">{{ $item->{$column['column']}  }}</a>
+                            @endif
+                        @else
+                            {!! $item->{$column['column']}  !!}
+                        @endif
+                    </td>
+                @endforeach
+                <td>
+                    <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
+                        <div class="btn-group mr-2" role="group" aria-label="First group">
+                            @if(Gate::check($permissions['update']))
+                                <a href="{{ url( $resource_route . '/' . $item->id . '/edit' ) }}" class="btn btn-sm btn-secondary"><i class="fa fa-edit"></i></a>
+                            @endif
+                            @if(Gate::check($permissions['delete']))
+                                @include('admin::partials.buttons._delete_button', [
+                                    'route'         => $resource_route . '/' . $item->id,
+                                    'text'   => '',
+                                    'icon' => '<i class="fa fa-trash"></i>'
+                                ])
+                            @endif
+                        </div>
+                    </div>
+                </td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
+<div class="table-footer">
+    {!! $items->appends(['sortBy' => $sort_by, 'order' => $order])->render() !!}
+</div>
